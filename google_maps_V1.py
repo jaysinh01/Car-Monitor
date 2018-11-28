9323a73a959035fc2a521889b0483fdb15eb8ecb
jaysinh01 <jaysinh@ualberta.ca>
2018-11-28 09:40:04 -0700

    Basic features have been implemented. Next step is work on GUI and then add more features

--- google_maps_V1.py
+++ google_maps_V1.py
@@ -19,62 +19,62 @@ def the_url(elements_dictionary):
     json_object = simplejson.loads(response)
     # response = requests.get(url).json()
     with open(elements_dictionary["origin"] + elements_dictionary["destination"] + ".json", "w") as result:
-            simplejson.dump(json_object, result)
+        simplejson.dump(json_object, result)
     with open("history.txt", "a+") as history:
-            history.write(elements_dictionary["origin"] + "\n")
-            history.write(elements_dictionary["destination"] + "\n")
+        history.write(elements_dictionary["origin"] + "\n")
+        history.write(elements_dictionary["destination"] + "\n")
     return json_object
 
 
 def main():
     """ THis function will generate tokens needed to to generate a request url """
-# all the requests will be stored in here
+    # all the requests will be stored in here
     url_dictionary = {}
-# Asking for the option
+    # Asking for the option
     choice = (input("would you prefer Bus or Car? "))
     print("Excellent")
     print("please enter your address\nEx: 1109 59a street SouthWest T6X0T2 Edmonton AB")
     origin_string = input(" ")
     # Replacing and editing input to use it effortlessly for URL requests.
-    origin = origin_string.replace(" ","+")
+    origin = origin_string.replace(" ", "+")
     url_dictionary["origin"] = origin.lower()
     destination_string = input("Please enter your destination. Ensure format is similar to the example shown above:\n")
     destination = destination_string.replace(" ","+")
     url_dictionary["destination"] = destination.lower()
     url_dictionary["mode"] = choice.lower()
-    try:
-      with open(url_dictionary["origin"] + url_dictionary["destination"] + ".json") as pervious_search:
-        json_string = json.loads(pervious_search)
-        reading_from_json(json_string)
-    except:
-      try:
-        result_dict = the_url(url_dictionary)
-        with open(url_dictionary["origin"] + url_dictionary["destination"] + ".json", "w") as new_file:
-          reading_from_json(result_dict)
 
-      except:
-        print("Please check the address again")
+    try:
+        with open(url_dictionary["origin"] + url_dictionary["destination"] + ".json") as pervious_search:
+            json_string = json.load(pervious_search) ## previous file does not open
+            reading_from_json(json_string)
+    except FileNotFoundError:
+        result_dict = the_url(url_dictionary)
+        ## implement autocomplete feture
+        reading_from_json(result_dict)
 
 
 def reading_from_json(json_file):
     ''' this function will read desired information from JSON file '''
-
     total_time = 0
     total_distance = 0
+
     for row in json_file["routes"][0]["legs"][0]["steps"]:
         direction = row['html_instructions']
         distance = row['distance']['text']
         duration = row['duration']['text']
-        total_time += duration
-        total_distance += distance
-        html_tags = re.findall('<[^<]+?>', direction)
+        time = re.findall("\d+", duration)
+        total_time += int(time[0])
+        # https://stackoverflow.com/questions/4703390/how-to-extract-a-floating-number-from-a-string
+        length_of_road = re.findall("\d+\.\d+", distance)
+        total_distance += float(length_of_road)
+        html_tags = re.findall("<[^<]+?>", direction)
         for remove in html_tags:
             direction = direction.replace(remove, "")
         print(direction + " in " + distance + " in about " + duration)
-    print("The total distance is " + total_distance)
-    print("The total duration is " + total_time)
+    print("The total distance is " + str(total_distance) + " km")
+    print("The total duration is " + str(total_time) + " mins")
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
